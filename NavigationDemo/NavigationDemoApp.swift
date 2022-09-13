addad1db27e92efab40aea894ca03b6dd4ef734a
Julia Nikitina <julianikitina.ios@gmail.com>
2022-09-13 23:12:50 +0300

    Initial Commit

---
+++ NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by Юлия Никитина on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
